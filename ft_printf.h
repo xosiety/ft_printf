d51a13d372e613f919c129bbbc327d37ea246610
afabbri <afabbri@student.42roma.it>
2023-03-07 15:36:06 +0100

    makefile + edit

--- ft_printf.h
+++ ft_printf.h
@@ -6,16 +6,16 @@
 /*   By: afabbri <afabbri@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 13:51:57 by afabbri           #+#    #+#             */
-/*   Updated: 2023/03/07 13:59:43 by afabbri          ###   ########.fr       */
+/*   Updated: 2023/03/07 14:09:52 by afabbri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-#ifndef ft_printf_h
-#define ft_printf_h
+# include <stdarg.h>
+# include <unistd.h>
 
-#include <stdarg.h>
-#include "libft.h"
+int	ft_printf(const char *format, ...);
 
-int ft_printf(const char *format, ...);
-int 
+#endif
